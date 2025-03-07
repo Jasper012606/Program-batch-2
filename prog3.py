ed4d023b9e801af6ba4b2b9eb0be0de5db7c3d8f
Jasper012606 <japeemga@gmail.com>
2025-03-07 20:14:17 +0800

    Update prog3.py

---
+++ prog3.py
@@ -0,0 +1,2 @@
+# A program that asks the user to enter 2 numbers and prints the difference of the two numbers
+print("Difference: ", int(input("Enter a number: " )) - int(input("Enter another number: ")))
