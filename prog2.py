a894f10868a9a2b64609cfedc6f11439aff8645f
Jasper012606 <japeemga@gmail.com>
2025-03-07 20:13:28 +0800

    Update prog2.py

---
+++ prog2.py
@@ -0,0 +1,7 @@
+# A program that asks the user to enter 2 numbers and prints "Not Equal" if the numbers are not the same
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+if num1 != num2:
+    print("Not Equal")
+else:
+    print("Equal")
