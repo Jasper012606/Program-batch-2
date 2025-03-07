b70d07a0d09e4f24552392ff0fed021d41a6de9a
Jasper012606 <japeemga@gmail.com>
2025-03-07 20:14:01 +0800

    Update prog1.py

---
+++ prog1.py
@@ -0,0 +1,8 @@
+# A program that asks user to enter 2 numbers and prints the smaller number
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+if num1 < num2:
+    print("The smaller number is", num1)
+else:
+    print("The smaller number is", num2)
