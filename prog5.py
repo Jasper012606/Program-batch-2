b9d6c43896ecf6486430ab944a0842fcdf0c0502
Jasper012606 <japeemga@gmail.com>
2025-03-07 20:14:37 +0800

    Update prog5.py

--- prog5.py
+++ prog5.py
@@ -1,1 +1,2 @@
-print(14% 5)
+# A program that asks the user to input 2 numbers and prints the remainder when the first number is divided by the second number
+print("Remainder: ", int(input("Enter a number: ")) % int(input("Enter another number: ")))
